17c9038cb48d28c9f523a3a6354a2335d61f57e7
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2024-02-09 10:45:55 -0800

    updates for API dups emails

--- src/test/java/primarycare/tests/Selenium/Patient_Attachments_SF_Admin_Interface/AdminInterface_Register_Person_In_Care_Attached_SYSAdmin.java
+++ src/test/java/primarycare/tests/Selenium/Patient_Attachments_SF_Admin_Interface/AdminInterface_Register_Person_In_Care_Attached_SYSAdmin.java
@@ -35,6 +35,7 @@ public class AdminInterface_Register_Person_In_Care_Attached_SYSAdmin extends Ba
     private String primaryContactName = "Igor PrimaryContactName";
 
     private String email = "accountToDelete@phsa.ca";
+    private String email_search_API = "";//"accountToDelete@phsa.ca";
     private String mobilePhone = "7788797899";
     private String currentFamilyDoctor = "Lori-Ann May Bus";
     private String cityOrTown = "Richmond";
@@ -56,7 +57,7 @@ public class AdminInterface_Register_Person_In_Care_Attached_SYSAdmin extends Ba
         log("Target Environment: "+ Utils.getTargetEnvironment());
 
         log("/*0.---Pre-conditioning API call to remove duplicate Patient account if found--*/");
-        ApiQueries.apiCallToRemovePatientAccount(email, legalLastName, legalFirstName);
+        ApiQueries.apiCallToRemovePatientAccount(email_search_API, legalLastName, legalFirstName);
 
 
         log("/*1.--- Login as an SysAdmin to Health Cloud Console of SF Admin side --*/");
