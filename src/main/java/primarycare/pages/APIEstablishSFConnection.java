6a4c50d4bf84700a34bf49ee5082d8b47bedceeb
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2024-07-04 11:39:32 -0700

    QAX: Refreshment for Sprint 32 - Patient

--- src/main/java/primarycare/pages/APIEstablishSFConnection.java
+++ src/main/java/primarycare/pages/APIEstablishSFConnection.java
@@ -11,8 +11,8 @@ public class APIEstablishSFConnection {
                 given().
                     urlEncodingEnabled(true)
                     .param("grant_type","password")
-                    .param("client_id","3MVG9696SH3dTknJmN3fE1Fpc3epKlh5tYp5Y_8Xg15C0R_Sa1S7ri5jjflULCe8KcuWkdQx4YwHUMjVE67T0")
-                    .param("client_secret","55BAC181AD2316D5AFC25611439E04734FD6D291EDB8C37B848E803D70B8AA84")
+                    .param("client_id","3MVG9TZvGM_0NqB0lblG48m.6T6ucgM1rxhs3sr5ubcM498ju3Wi4NmLFniacHcU7WzRzYnor4EcTdvn7jx7E")
+                    .param("client_secret","DE161A2E5C59C0AB8131A0138BBA87EDD570E712E8EDC02E222D62A33E33D951")
                     .param("username","igor.emelyanov@phsa.ca.hlthbcqax")
                     .param("password","Technology1990!!!!!!")
                     .header("Accept","application/json")
