f0a89f659ffd7b7ca45d730a4ba03ed71f94b4e5
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2024-07-13 00:53:39 -0700

    QAX: back to ops.addArguments("--headless"); // Run in headless mode if you're running on a CI server                      ops.addArguments("--disable-gpu");                      ops.addArguments("--no-sandbox"); //this flag killing 100% CPU//https://github.com/SeleniumHQ/selenium/issues/13872                      ops.addArguments("--remote-allow-origins=*");

--- src/test/java/primarycare/tests/BaseTest_PrimaryCare.java
+++ src/test/java/primarycare/tests/BaseTest_PrimaryCare.java
@@ -51,9 +51,9 @@ public class BaseTest_PrimaryCare {
         ////
         ///// for Windows local and Jenkins
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--no-sandbox"); //this flag killing 100% CPU//https://github.com/SeleniumHQ/selenium/issues/13872
         options.addArguments("--headless"); // Run in headless mode if you're running on a CI server
         options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox"); //this flag killing 100% CPU//https://github.com/SeleniumHQ/selenium/issues/13872
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
