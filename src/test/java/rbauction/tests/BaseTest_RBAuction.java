e169ecd826a25e9c2257be9080387e719f44282a
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2024-07-16 10:25:25 -0700

    RBAuction: adding "--headless" "--disable-gpu" "--no-sandbox" --remote-allow-origins=*"

--- src/test/java/rbauction/tests/BaseTest_RBAuction.java
+++ src/test/java/rbauction/tests/BaseTest_RBAuction.java
@@ -50,8 +50,9 @@ public class BaseTest_RBAuction {
         ///// for Windows local and Jenkins
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        //options.addArguments("--no-sandbox"); //this flag killing 100% CPU//https://github.com/SeleniumHQ/selenium/issues/13872
-        //options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-gpu");//????
+        options.addArguments("--no-sandbox"); //this flag killing 100% CPU//https://github.com/SeleniumHQ/selenium/issues/13872
+        options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         loginPage = new LoginPage(getDriver());
